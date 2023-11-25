4f8850af22f8129bbee180404d63cab085357a8c
Elaine <elaineakemi@gmail.com>
2023-11-25 15:03:55 +0000

    fix: add workaround to fix apple and checkpoint animation

--- Level1.cpp
+++ Level1.cpp
@@ -4,18 +4,6 @@ Level1::Level1() {}
 
 void Level1::Init()
 {
-
-    Item apple1(appleTexture, 17, {75.0f, 250.0f}, RAYWHITE, disappearTexture);
-    Item apple2(appleTexture, 17, {100.0f, 280.0f}, RAYWHITE, disappearTexture);
-    Item apple3(appleTexture, 17, {125.0f, 250.0f}, RAYWHITE, disappearTexture);
-    Item apple4(appleTexture, 17, {150.0f, 280.0f}, RAYWHITE, disappearTexture);
-    Item apple5(appleTexture, 17, {175.0f, 250.0f}, RAYWHITE, disappearTexture);
-
-    apples[0] = apple1;
-    apples[1] = apple2;
-    apples[2] = apple3;
-    apples[3] = apple4;
-    apples[4] = apple5;
 }
 
 void Level1::RenderBackground()
@@ -28,13 +16,17 @@ void Level1::RenderItems()
 {
     for (auto apple : apples)
     {
-        apple.Render();
+        apple->Render();
     }
 
-    for (Item item : checkpoints)
+    for (auto item : checkpoints)
     {
-        item.Render();
+        item->Render();
     }
+}
+
+void Level1::Update()
+{
 }
 
 void Level1::Unload()
--- Level1.h
+++ Level1.h
@@ -15,19 +15,27 @@ private:
     Texture2D appleTexture{LoadTexture("resources/textures/items/apple.png")};
     Texture2D checkpointTexture{LoadTexture("resources/textures/items/checkpoint.png")};
 
-    // TODO: check why lost animation
-    Item apples[5];
+    // workaround to fix apples animation
+    int applesCount = 5;
+    Item apple1{appleTexture, 17, {75.0f, 250.0f}, RAYWHITE, disappearTexture};
+    Item apple2{appleTexture, 17, {100.0f, 280.0f}, RAYWHITE, disappearTexture};
+    Item apple3{appleTexture, 17, {125.0f, 250.0f}, RAYWHITE, disappearTexture};
+    Item apple4{appleTexture, 17, {150.0f, 280.0f}, RAYWHITE, disappearTexture};
+    Item apple5{appleTexture, 17, {175.0f, 250.0f}, RAYWHITE, disappearTexture};
+    Item *apples[5]{&apple1, &apple2, &apple3, &apple4, &apple5};
 
-    // TODO: same as apples, lost animation
-    Item checkpoints[3]{Item(checkpointTexture, 10, {700.0f, 370.0f}, RAYWHITE, disappearTexture),
-                        Item(checkpointTexture, 10, {200.0f, 88.0f}, RAYWHITE, disappearTexture),
-                        Item(checkpointTexture, 10, {730.0f, 237.0f}, RAYWHITE, disappearTexture)};
+    // workaround to fix checkpoints animation
+    Item checkpoint1{checkpointTexture, 10, {700.0f, 370.0f}, RAYWHITE, disappearTexture};
+    Item checkpoint2{checkpointTexture, 10, {200.0f, 88.0f}, RAYWHITE, disappearTexture};
+    Item checkpoint3{checkpointTexture, 10, {730.0f, 237.0f}, RAYWHITE, disappearTexture};
+    Item *checkpoints[3]{&checkpoint1, &checkpoint2, &checkpoint3};
 
 public:
     Level1();
     void Init();
     void RenderBackground();
     void RenderItems();
+    void Update();
     void Unload();
 };
 
